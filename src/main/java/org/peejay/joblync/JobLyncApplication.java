ab9a855872f95a8156c5b58526d49175b6e40b69
Oderinde Suliha <oderindesuliha@gmail.com>
2025-07-01 21:51:34 +0100

    first commit

---
+++ src/main/java/org/peejay/joblync/JobLyncApplication.java
@@ -0,0 +1,13 @@
+package org.peejay.joblync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobLyncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobLyncApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/peejay/joblync/JobLyncApplicationTests.java
@@ -0,0 +1,13 @@
+package org.peejay.joblync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobLyncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
